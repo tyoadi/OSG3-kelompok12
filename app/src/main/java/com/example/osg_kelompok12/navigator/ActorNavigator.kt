b66b636ddfb7c5432a63350e26fd7bc1272cadb0
Myself <sulistyo.adi32@gmail.com>
2019-03-22 05:10:27 +0700

    viemodel & navigator

--- app/src/main/java/com/example/osg_kelompok12/navigator/ActorNavigator.kt
+++ app/src/main/java/com/example/osg_kelompok12/navigator/ActorNavigator.kt
@@ -1,2 +1,9 @@
 package com.example.osg_kelompok12.navigator
 
+import com.example.osg_kelompok12.model.ResultsItem
+
+interface ActorNavigator {
+
+    fun loadListActor(data: List<ResultsItem>)
+    fun errorLoadListActor(messages: String)
+}
--- app/src/main/java/com/example/osg_kelompok12/view/MainActivity.kt
+++ app/src/main/java/com/example/osg_kelompok12/view/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.osg_kelompok12.view
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.osg_kelompok12.R
+import com.example.osg_kelompok12.model.ResultsItem
+import com.example.osg_kelompok12.navigator.ActorNavigator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ActorNavigator {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun loadListActor(data: List<ResultsItem>) {
+
+    }
+
+    override fun errorLoadListActor(messages: String) {
+
     }
 }
